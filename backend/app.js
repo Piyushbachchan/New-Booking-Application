bc51b104c65c67c52ec5f2a7f71ecbbc3065687c
Piyushyo <piyushbachchan2@gmail.com>
2023-05-12 16:42:22 +0530

    first commit

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+const userRouter = require('./routes/user-routes');
+dotenv.config();
+
+
+///midddleware
+app.use("/user", userRouter);
+
+mongoose.connect("mongodb+srv://piyushbachchan2:G67ovdizjbLTClsU@movie-system.xzdybs6.mongodb.net/Movies?retryWrites=true&w=majority")
+
+app.listen(3500, () => {
+
+    console.log(`Connected to localhost port ${3500}`);
+})
